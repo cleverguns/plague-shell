aac1f144ed885830dd1295b6b5324f77adbf0948
FonderElite <s1170044@usls.edu.ph>
2022-01-05 08:26:01 +0000

    Delete plague-shell/plague-shell directory

--- plague-shell/plague-shell/hardware-info/hardware-info.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-source /home/user/plague-shell/plague-shell/colors.sh
-function hardware_info(){
-OS="$(uname -n)"
-KERNEL="$(uname -r)"
-echo -e "\n\n${BWhite}      OS: ${BGreen} ${OS}"
-echo -e "     ${BWhite} Kernel: ${BGreen} ${KERNEL}"
-}
-
